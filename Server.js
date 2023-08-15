4d72abf46762d0a71a3ddaed9fd04211e177506c
Syed Saif Ali Abidi <saifaliabidi@gmail.com>
2023-08-15 18:01:40 +1000

    commit

--- Server.js
+++ Server.js
@@ -1,63 +1,81 @@
-let express = require('express');
-let app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const path = require('path');
+
 const uri = "mongodb+srv://saifaliabidi:saifaliabidi@cluster0.zzg0otj.mongodb.net/?retryWrites=true&w=majority";
-let port = process.env.port || 3000;
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 let collection;
 
-app.use(express.static(__dirname + '/'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 async function runDBConnection() {
     try {
         await client.connect();
         collection = client.db().collection('Cat');
-        console.log(collection);
-    } catch(ex) {
+        console.log('Connected to MongoDB');
+    } catch (ex) {
         console.error(ex);
     }
 }
 
+
+async function getAllCats() {
+    try {
+        const cats = await collection.find({}).toArray();
+        return cats;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+async function postCat(cat) {
+    try {
+        const result = await collection.insertOne(cat);
+        return result;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/api/cats', (req, res) => {
-    getAllCats((err,result) => {
-        if (!err) {
-            res.json({statusCode:200, data:result, message:'get all cats success'});
-        }
-    });
+
+app.listen(port, async () => {
+    console.log(`Express server started on port ${port}`);
+    await runDBConnection();
 });
 
-app.post('/api/cat', (req, res) => {
-    let cat = req.body;
-    postCat(cat, (err, result) => {
-        if (!err) {
-            res.json({statusCode:201, data:result, message:'success'});
-        }
-    });
+app.get('/api/cats', async (req, res) => {
+    try {
+        const result = await getAllCats();
+        res.json({ statusCode: 200, data: result, message: 'get all cats success' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ statusCode: 500, message: 'Internal server error' });
+    }
 });
 
-function postCat(cat,callback) {
-    collection.insertOne(cat,callback);
-}
-
-function getAllCats(callback) {
-    collection.find({}).toArray(callback);
-}
-
-app.listen(3000, ()=>{
-    console.log('express server started');
-    runDBConnection();
-}); 
+app.post('/api/cat', async (req, res) => {
+    try {
+        const cat = req.body;
+        const result = await postCat(cat);
+        res.json({ statusCode: 201, data: result, message: 'success' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ statusCode: 500, message: 'Internal server error' });
+    }
+});
--- public/JS/Scripts.js
+++ public/JS/Scripts.js
@@ -1,3 +1,27 @@
+// const addCards = (items) => {
+//     items.forEach((item) => {
+//         let itemToAppend =
+//             '<div class="col s4 center-align">' +
+//             '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
+//             item.path +
+//             '">' +
+//             '</div><div class="card-content">' +
+//             '<span class="card-title activator grey-text text-darken-4">' +
+//             item.title +
+//             '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
+//             "</a></p></div>" +
+//             '<div class="card-reveal">' +
+//             '<span class="card-title grey-text text-darken-4">' +
+//             item.subTitle +
+//             '<i class="material-icons right">close</i></span>' +
+//             '<p class="card-text">' +
+//             item.description +
+//             "</p>" +
+//             "</div></div></div>";
+//         $("#card-section").append(itemToAppend);
+//     });
+// };
+
 const addCards = (items) => {
     items.forEach((item) => {
         let itemToAppend =
@@ -22,16 +46,30 @@ const addCards = (items) => {
     });
 };
 
+
 const submitForm = () => {
-    let formData = {};
-    formData.title = $('#title').val();
-    formData.subTitle = $('#subTitle').val();
-    formData.path = $('#path').val();
-    formData.description = $('#description').val();
+    let formData = {
+        title: $('#title').val(),
+        subTitle: $('#subTitle').val(),
+        path: $('#path').val(),
+        description: $('#description').val()
+    };
 
     console.log(formData);
-    postCat(formData);
-}
+
+    $.ajax({
+        url: '/api/cat',
+        type: 'POST',
+        data: formData,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('cat post successful');
+                getAllCats();
+            }
+        }
+    });
+};
+
 
 function postCat(cat){
     $.ajax({
@@ -46,13 +84,18 @@ function postCat(cat){
     });
 }
 
-function getAllCats(){
+function getAllCats() {
     $.get('/api/cats', (response) => {
-        if (response.statusCode === 200){
+        console.log(response); 
+        if (response.statusCode === 200) {
             addCards(response.data);
         }
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+        console.error(jqXHR.responseText);
     });
 }
+
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
